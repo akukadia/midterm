2e87fdd32a8da17167581a2aec14f3ed04b0cf91
Akash <kukadia@myseneca.ca>
2021-06-25 12:08:37 -0400

    midterm

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Banner from './components/Banner';
+import Controls from './components/Controls';
+
+function App() {
+  return (
+    <div className="app">
+      <Banner/>
+      <Controls/>
+    </div>
+  )
+}
+
+export default App
+ 
